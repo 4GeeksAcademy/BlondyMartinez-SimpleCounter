7d9cf75ecc8ff8b8a5b787f1aa009075ff27e90b
Blondy Martínez <47917655+BlondyMartinez@users.noreply.github.com>
2024-03-25 18:02:48 +0000

    changed class in icons to className

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -14,9 +14,9 @@ function Home(props){
 				<h1 className="counter-text">{props.counter}</h1>
 			</div>
 			<div className="mt-5">
-				<button className="btn ms-3 fs-1 control-button" onClick={() => props.stop()}><i class="fa-solid fa-stop"></i></button>
-				<button className="btn ms-3 fs-1 control-button" onClick={() => props.resume()}><i class="fa-solid fa-play"></i></button>
-				<button className="btn ms-3 fs-1 control-button" onClick={() => props.restart()}><i class="fa-solid fa-arrow-rotate-left"></i></button>
+				<button className="btn ms-3 fs-1 control-button" onClick={() => props.stop()}><i className="fa-solid fa-stop"></i></button>
+				<button className="btn ms-3 fs-1 control-button" onClick={() => props.resume()}><i className="fa-solid fa-play"></i></button>
+				<button className="btn ms-3 fs-1 control-button" onClick={() => props.restart()}><i className="fa-solid fa-arrow-rotate-left"></i></button>
 			</div>
 			<div className="mt-5">
 				<input className="input-bar" ref={inputRef} id="search-bar" type="number" placeholder="Number of seconds."></input>	
