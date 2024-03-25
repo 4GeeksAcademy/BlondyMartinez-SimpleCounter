2498ebf0d14b05471a53e0568630ba03ad919035
Blondy Martínez <47917655+BlondyMartinez@users.noreply.github.com>
2024-03-25 08:54:01 +0000

    added countdown input and buttonç

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,10 +1,18 @@
-import React from "react";
+import React, { useRef } from "react";
 
-//create your first component
 function Home(props){ 
+    const inputRef = useRef(null);
+
 	return (
-		<div className="text-center">
-			<h1 className="counter-text">{props.counter}</h1>
+		<div className="d-flex flex-column align-items-center justify-content-center">
+			<div className="text-center my-3">
+				<h1 className="counter-title">{props.title}</h1>
+				<h1 className="counter-text">{props.counter}</h1>
+			</div>
+			<div className="mt-5">
+				<input className="input-bar" ref={inputRef} id="search-bar" type="number" placeholder="Number of seconds."></input>	
+				<button className="btn btn-primary ms-3" onClick={() => props.setCountdown(inputRef.current.value)}>Set Countdown</button>
+			</div>
 		</div>
 	);
 };
